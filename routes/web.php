f02f8433d50b5e70d15971051f32a6ce6e9d716d
Gaojy <340067958@qq.com>
2018-10-24 17:27:40 +0800

    finish user auth

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,19 @@
 <?php
 
 Route::get('/', 'PagesController@root')->name('pages.root');
+
+//与以下9个合并同
+//Auth::routes();
+//1.Authentication Routes  注册
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+//2.Registeration Routes  登录
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+//3.Password Routes 密码操作
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendRequestForm')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
